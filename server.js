04cfa87c5174f38f5cecdf0488bcb7eaf4ffa255
timsilby <69242373+timsilby@users.noreply.github.com>
2020-10-29 09:54:08 +1000

    Added route and function to add new note to db.json.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Dependencies
 const express = require("express");
 const path = require("path");
+const { arrNotesData, addNote } = require("./lib/notesData");
 
 // Sets up the Express App
 const app = express();
@@ -9,6 +10,7 @@ const PORT = 3000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
 
 //=======================
@@ -21,11 +23,23 @@ app.get("/notes", function(req, res) {
 });
 
 
+app.get("/api/notes", function(req, res) {
+	res.json(arrNotesData);
+});
+
+
+app.post("/api/notes", function(req, res) {
+	addNote(req.body);
+	res.json(arrNotesData);
+});
+
+
+
+
 // Default route. Sends the home page.
 app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "./public/index.html"));
 });
-
 
 
 
