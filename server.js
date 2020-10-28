6bd91ccaf8428d46a7958dade0b877f4d626c323
timsilby <69242373+timsilby@users.noreply.github.com>
2020-10-28 18:48:47 +1000

    Added code for starting server and basic html routes.

---
+++ server.js
@@ -0,0 +1,36 @@
+// Dependencies
+const express = require("express");
+const path = require("path");
+
+// Sets up the Express App
+const app = express();
+const PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+//=======================
+// Routes
+//=======================
+
+// The /notes route sends the notes page to the browser.
+app.get("/notes", function(req, res) {
+	res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+
+// Default route. Sends the home page.
+app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+
+
+
+
+// Start the server
+app.listen(PORT, function() {
+	console.log("App listening on PORT " + PORT);
+});
